a61dc6a875a645fd5c67b9f2560b74e5e3880d3b
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-03-24 15:36:47 +0900

    proejct 구분

---
+++ project-2/cache/src/main/java/com/example/cache/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+
+package com.example.cache.domain.repository;
+
+import com.example.cache.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
