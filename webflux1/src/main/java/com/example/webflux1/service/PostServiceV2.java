f3f7e0ec62902fc82bb85fcc81d8ba35f4823e5d
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-04-08 20:42:44 +0900

    ch03 - 10. webflux

---
+++ webflux1/src/main/java/com/example/webflux1/service/PostServiceV2.java
@@ -0,0 +1,39 @@
+package com.example.webflux1.service;
+
+import com.example.webflux1.client.PostClient;
+import com.example.webflux1.dto.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceV2 {
+    private final PostClient postClient;
+
+    public Mono<PostResponse> getPostContent(Long id) {
+        return postClient.getPost(id)
+                .onErrorResume(error -> Mono.just(new PostResponse(id.toString(), "Fallback data %d".formatted(id))));
+        // 에러 발생했을때 생성자를 이용해서 에러가아닌 데이터를 반환
+    }
+
+    public Flux<PostResponse> getMultiplePostContent(List<Long> idList) {
+        return Flux.fromIterable(idList)
+                .flatMap(this::getPostContent)
+                .log();
+    }
+
+    public Flux<PostResponse> getParallelMultiplePostContent(List<Long> idList) {
+        return Flux.fromIterable(idList)
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .flatMap(this::getPostContent)
+                .log()
+                .sequential();
+    }
+
+}
