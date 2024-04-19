4e693cc7c30b8aec5ebf52a01ac649e61c1f143c
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-04-19 23:19:29 +0900

    ch04 - 05. register

--- flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -13,14 +13,29 @@ import java.time.Instant;
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimestamp = Instant.now().getEpochSecond();
+
+
         return reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString(), unixTimestamp)
                 .filter(i -> i)
                 .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERED_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
-                .map(i -> i >= 0 ? i+1: i);
+                .map(i -> i >= 0 ? i+1 : i);
+    }
+
+    public Mono<Long> allowUser(final String queue, final Long count) {
+        return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count)
+                .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
+                .count();
+    }
+
+    public Mono<Boolean> isAllowed(final String queue, final Long userId) {
+        return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue), userId.toString())
+                .defaultIfEmpty(-1L)
+                .map(rank -> rank >= 0);
     }
 }
