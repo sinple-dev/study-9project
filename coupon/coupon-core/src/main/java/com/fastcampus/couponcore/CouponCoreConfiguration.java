7871de777600c0d265fc33bdde9fd393c768bb33
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-05-06 22:27:29 +0900

    ch02 - 01. config

---
+++ coupon/coupon-core/src/main/java/com/fastcampus/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,10 @@
+package com.fastcampus.couponcore;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class CouponCoreConfiguration {
+
+}
